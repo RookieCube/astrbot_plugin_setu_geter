7ebcf4d4293562c52c63fd1d7b9041b729577fe2
RookieCube <146699250+RookieCube@users.noreply.github.com>
2025-08-26 23:55:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,17 +5,14 @@ from astrbot.api import logger
 class Setu(Star):
     def __init__(self, context: Context):
         super().__init__(context)
-
     async def initialize(self):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
-    
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("add")
-    def add(self, event: AstrMessageEvent, a: str, b: int, c: str):
+    @filter.command("setu")
+    def setu(self, event: AstrMessageEvent, a: str, b: int, c: str):
         chain = [
             Comp.At(qq=event.get_sender_id()), # At 消息发送者
             Comp.Image.fromURL(f"https://image.anosu.top/pixiv/direct?keyword={a}&r18={b}&size={c}"), # 从 URL 发送图片
         ]
-    yield event.chain_result(chain)
+        yield event.chain_result(chain)
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
